e7835b221192693f5526fa981d22481fd44306f9
PurpleSavage <jzuritapalomino@gmail.com>
2025-06-04 00:37:29 -0500

    arrgealr header

--- src/views/landing/Header.tsx
+++ src/views/landing/Header.tsx
@@ -8,20 +8,24 @@ function Header() {
         <source src="/black-hole.mp4" type="video/webm" />
         Tu navegador no soporta la reproducción de videos.
       </video>
-     <div className="absolute overflow-hidden top-0 left-0 right-0 bottom-0 flex items-center 
-     justify-center flex-col gap-6 bg-black/40">
-        <div className={`${style.meteorContainer} rotate-90`}>
-            <div className={`${style.meteor}`}></div>
+      <div className="absolute top-0 left-0 right-0 bottom-0 z-10 overflow-hidden">
+        <div className={`${style.meteorContainer} z-20 rotate-90`}>
+            <div className={`${style.meteor}`}></div> 
             <div className={`${style.meteor}`}></div>
             <div className={`${style.meteor}`}></div>
             <div className={`${style.meteor}`}></div>
             <div className={`${style.meteor}`}></div>
         </div>
+      </div>
+     <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center 
+     justify-center flex-col gap-6 bg-black/40  z-30">
+        
         <h1 className="text-7xl text-white font-bold animate-pulse">GENERA IMÁGENES ÉPICAS</h1>
         <p className="text-xl text-gray-300 md:w-3/4 lg:w-3/4 font-bold text-center">Transforma conceptos en arte visual revolucionario. 
           Nuestra IA de última generación combina algoritmos RGB avanzados para crear imágenes que desafían la realidad.</p>
 
-        <Link href="" className="flex px-6 gap-2 items-center py-2 bg-white text-black rounded-md">
+        <Link href="" className="flex px-6 gap-2 items-center py-2 bg-white 
+        text-black rounded-md hover:bg-slate-200 cursor-pointer">
           Iniciar generación
           <FaArrowRight size={20}/>
         </Link>
