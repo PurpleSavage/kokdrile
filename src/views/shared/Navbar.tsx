20f80b1f04afa2fe18a6e2f207e77ebba73c765e
PurpleSavage <jzuritapalomino@gmail.com>
2025-06-04 02:30:07 -0500

    arregalr bug en navbar

--- src/views/shared/Navbar.tsx
+++ src/views/shared/Navbar.tsx
@@ -9,12 +9,12 @@ function Navbar() {
         <Link href="/" className="text-2xl font-bold text-white grow ">Kokodrile</Link>
         <div className="space-x-6">
             {
-              path==='/auth/register' ? 
+              path==='/auth/register' || path==='/' ? 
               <Link href="/auth/register" className="px-6 py-2 hover:bg-slate-200 font-medium rounded-md bg-white
              text-black">Register</Link> : null
             }
             {
-              path === '/auth'? 
+              path === '/auth' || path==='/'? 
               <Link href="/auth" className="px-6 py-2 hover:text-slate-300 font-medium rounded-md border border-slate-600/50
              text-white">Sign in</Link> : null
             }
